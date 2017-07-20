adfb8ee64aed7f55772ae5b2961085aa468ff1d8
Erjon Lamy <erjon.lamy@outlook.fr>
2017-07-20 11:25:52 +0200

    blibli

--- android/app/src/main/java/com/reactnativesample/MainApplication.java
+++ android/app/src/main/java/com/reactnativesample/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnativesample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import io.sentry.RNSentryPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
             new RNSentryPackage(MainApplication.this)
       );
     }
